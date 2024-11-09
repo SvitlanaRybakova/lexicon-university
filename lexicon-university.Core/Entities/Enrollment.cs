c639f493fd6c6f67c77f3aff4aa938e5947e6ac5
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-09 11:05:34 +0100

    feat: add course, student and enrollment entities

---
+++ lexicon-university.Core/Entities/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lexicon_university.Core.Entities
+{
+    internal class Enrollment
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int Grade { get; set; }
+    }
+}
---
+++ lexicon-university.Core/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lexicon_university.Core.Entities
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string Email { get; set; }
+    }
+}
