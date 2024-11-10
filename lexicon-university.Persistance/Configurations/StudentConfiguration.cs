f8fb1c34bdb3f8b3b13ca068f0ce702947c1c874
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-10 18:12:10 +0100

    refactor: add student configuration

---
+++ lexicon-university.Persistance/Configurations/StudentConfiguration.cs
@@ -0,0 +1,27 @@
+﻿
+using lexicon_university.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lexicon_university.Persistance.Configurations
+{
+    public class StudentConfiguration: IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder
+                .HasMany(s => s.Courses)
+                .WithMany(c => c.Students)
+                .UsingEntity<Enrollment>(
+                e => e.HasOne(e => e.Course).WithMany(c => c.Enrollments),
+                e => e.HasOne(e => e.Student).WithMany(s => s.Enrollments),
+                e => e.HasKey(e => new { e.CourseId, e.StudentId }));
+        }
+    }
+}
--- lexicon-university.Persistance/Data/LexiconUniversityContext.cs
+++ lexicon-university.Persistance/Data/LexiconUniversityContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using lexicon_university.Core.Entities;
+using lexicon_university.Persistance.Configurations;
 
 namespace lexicon_university.Persistance.Data
 {
@@ -22,13 +23,7 @@ namespace lexicon_university.Persistance.Data
             //base.OnModelCreating(modelBuilder);
             //modelBuilder.Entity<Enrollment>().HasKey(e => new { e.CourseId, e.StudentId });
 
-            modelBuilder.Entity<Student>()
-                .HasMany(s => s.Courses)
-                .WithMany(c => c.Students)
-                .UsingEntity<Enrollment>(
-                e => e.HasOne(e => e.Course).WithMany(c => c.Enrollments),
-                e => e.HasOne(e => e.Student).WithMany(s => s.Enrollments),
-                e => e.HasKey(e => new { e.CourseId, e.StudentId }));
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
         }
     }
 }
