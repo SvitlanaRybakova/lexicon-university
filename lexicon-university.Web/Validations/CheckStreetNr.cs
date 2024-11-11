5a1ce581b5b159b79a33602a8464ef47dd1ed084
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-11 19:58:28 +0100

    feat: add the FE validation

--- lexicon-university.Web/Validations/CheckStreetNr.cs
+++ lexicon-university.Web/Validations/CheckStreetNr.cs
@@ -1,14 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace lexicon_university.Web.Validations
 {
-    public class CheckStreetNr : ValidationAttribute
+    public class CheckStreetNr : ValidationAttribute, IClientModelValidator
     {
         private readonly int max;
+
         public CheckStreetNr(int max)
         {
             this.max = max;
         }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-streetnr", "Not a valid street nr");
+            context.Attributes.Add("data-val-streetnr-max", $"{max}");
+        }
+
         public override bool IsValid(object? value)
         {
             if (value is string input)
---
+++ lexicon-university.Web/wwwroot/js/StreetNrValidation.js
@@ -0,0 +1,9 @@
+﻿$.validator.addMethod('streetnr', function (value, element, params) {
+    let nr = value.split(' ').pop()
+    let res = parseInt(nr)
+    if (isNaN(res)) return false
+    let max = parseInt(params)
+    if (isNaN(max)) return false
+    return res <= max
+})
+$.validator.unobtrusive.adapters.addSingleVal('streetnr', 'max')
